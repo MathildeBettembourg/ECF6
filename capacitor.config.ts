3c0166bfb30ea243e153db7c5db1ef5c8b35f6f4
Administrateur <mathilde.bettembourg@gmail.com>
2023-02-02 09:05:03 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'efc6',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
